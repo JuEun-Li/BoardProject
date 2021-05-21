83044870cd2e1eb7299eac44bcd5ba374e10aa46
Li JuEun <wndms61@gmail/com>
2021-05-21 11:23:33 +0900

    스크립트 막기

--- board/src/main/java/com/board/domain/BoardVO.java
+++ board/src/main/java/com/board/domain/BoardVO.java
@@ -22,19 +22,19 @@ public class BoardVO {
 		this.bno = bno;
 	}
 	public String getTitle() {
-		return title;
+		return title.replaceAll("(?i)<script", "&lt;script");
 	}
 	public void setTitle(String title) {
 		this.title = title;
 	}
 	public String getContent() {
-		return content;
+		return content.replaceAll("(?i)<script", "&lt;script");
 	}
 	public void setContent(String content) {
 		this.content = content;
 	}
 	public String getWriter() {
-		return writer;
+		return writer.replaceAll("(?i)<script", "&lt;script");
 	}
 	public void setWriter(String writer) {
 		this.writer = writer;
